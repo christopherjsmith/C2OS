00039fe61a9b9d39d520b94cf1a142ed62e9acc7
christopherjsmith <christopher.james.dev@gmail.com>
2013-04-08 14:52:39 +0100

    moved

--- kernel/utils/includes/memory.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MEMORY_H
-#define __MEMORY_H
-
-void memcopy(char* src, char* dst, int numberOfBytes);
-
-#endif
--- kernel/utils/memory.c
+++
@@ -1,7 +0,0 @@
-#include "includes/memory.h"
-
-void memcopy(char* src, char* dst, int numberOfBytes) {
-    for( ; numberOfBytes > 0; numberOfBytes--) {
-        *(dst++) = *(src++);
-    }
-}
